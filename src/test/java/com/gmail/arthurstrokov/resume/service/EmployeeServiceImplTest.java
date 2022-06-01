0d9553667d7162f87a5375d253556b22724967c3
Arthur Strokov <arthurstrokov@gmail.com>
2022-06-02 00:53:25 +0300

    up tests

--- src/test/java/com/gmail/arthurstrokov/resume/service/EmployeeServiceImplTest.java
+++ src/test/java/com/gmail/arthurstrokov/resume/service/EmployeeServiceImplTest.java
@@ -31,8 +31,8 @@ import static org.mockito.Mockito.*;
 @AutoConfigureMockMvc
 class EmployeeServiceImplTest {
 
-    @Autowired
-    SpecificationServiceImpl specificationServiceImpl;
+    @MockBean
+    SpecificationService specificationService;
     @MockBean
     EmployeeRepository employeeRepository;
     @Autowired
@@ -117,7 +117,7 @@ class EmployeeServiceImplTest {
 
     @Test
     void getFiltered() {
-        Specification<Employee> spec = specificationServiceImpl.employeeRequestToSpecification("email:arthurstrokov@gmail.com");
+        Specification<Employee> spec = specificationService.employeeRequestToSpecification("email:arthurstrokov@gmail.com");
         employeeRepository.save(employee);
         when(employeeRepository.findAll(spec)).thenReturn(employeeList);
         List<EmployeeDTO> employeesFiltered = employeeService.getAllByFilter("email:arthurstrokov@gmail.com");
@@ -128,7 +128,7 @@ class EmployeeServiceImplTest {
 
     @Test
     void getAllFilteredAndPageable() {
-        Specification<Employee> spec = specificationServiceImpl.employeeRequestToSpecification("email:arthurstrokov@gmail.com");
+        Specification<Employee> spec = specificationService.employeeRequestToSpecification("email:arthurstrokov@gmail.com");
         Pageable pageable = PageRequest.of(0, 5, Sort.by("email"));
         employeeRepository.save(employee);
         when(employeeRepository.findAll(spec, pageable)).thenReturn(Page.empty());
