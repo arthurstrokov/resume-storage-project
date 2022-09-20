e1f65a9637054c2079753052d38a8b4b26e9fcd6
arthurstrokov <arthurstrokov@gmail.com>
2022-09-20 15:18:19 +0300

    up ActuatorController

--- src/main/java/com/gmail/arthurstrokov/resume/controller/ActuatorController.java
+++ src/main/java/com/gmail/arthurstrokov/resume/controller/ActuatorController.java
@@ -1,7 +1,10 @@
 package com.gmail.arthurstrokov.resume.controller;
 
+import io.micrometer.core.annotation.Timed;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +14,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 @RequestMapping("/actuator/health")
 public class ActuatorController {
-
+    @Timed(value = "get_actuator_health", histogram = true, percentiles = 0.95)
     @GetMapping
-    public String actuator() {
-        return "/actuator/health";
+    public ResponseEntity<String> actuator() {
+        return new ResponseEntity<>("/actuator/health", HttpStatus.OK);
     }
 }
